e47117f6dd167376ecc7905d39e23283a6c8fd2d
Farid ullah <faridullah996@gmail.com>
2022-10-23 08:44:21 +0500

    navigation based on login state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'night-cafe-clone';
+export class AppComponent implements OnInit {
+  	title = 'night-cafe-clone';
+
+	constructor(private authService: AuthService,
+				private router: Router) { }
+
+	ngOnInit(): void {
+		if (!this.authService.isLoggedIn) {
+			this.router.navigateByUrl('login');
+		}
+	}
 }
